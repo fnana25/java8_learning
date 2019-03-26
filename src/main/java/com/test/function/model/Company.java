e88fb9a4bc369ff2ccb32a8c1a406a445b9da14a
冯娜 <fengna@iwubida.com>
2019-03-26 15:24:09 +0800

    Optional use

---
+++ src/main/java/com/test/function/model/Company.java
@@ -0,0 +1,21 @@
+package com.test.function.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Company
+ *
+ * @author fengna
+ * @date 2019/3/26
+ */
+@Data
+@Builder
+public class Company {
+
+    private String name;
+
+    private List<Employee> employees;
+}
