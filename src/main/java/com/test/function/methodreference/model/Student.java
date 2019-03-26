7db829a33594965788a6450f772a4dc0f14ed631
冯娜 <fengna@iwubida.com>
2019-03-26 16:37:32 +0800

    方法引用

---
+++ src/main/java/com/test/function/methodreference/model/Student.java
@@ -0,0 +1,43 @@
+package com.test.function.methodreference.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Student
+ *
+ * @author fengna
+ * @date 2019/3/26
+ */
+@Data
+@Builder
+public class Student {
+
+    private int id;
+
+    private String name;
+
+    private int score;
+
+    public static int staticCompareByName(Student a, Student b) {
+        return a.getName().compareToIgnoreCase(b.getName());
+    }
+
+    public static int staticCompareByScore(Student a, Student b) {
+        return b.getScore() - a.getScore();
+    }
+
+    public int compareByName(Student a) {
+        return this.getName().compareToIgnoreCase(a.getName());
+    }
+
+    public int compareByScore(Student a) {
+        return this.getScore() - a.getScore();
+    }
+
+    @Override
+    public String toString() {
+        return "" + this.id;
+    }
+}
