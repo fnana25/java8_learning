588006e8d24ca0b7410fcc72cf556442cfae731d
冯娜 <fengna@iwubida.com>
2019-03-26 15:24:23 +0800

    supplier use

---
+++ src/main/java/com/test/function/SupplierUse.java
@@ -0,0 +1,23 @@
+package com.test.function;
+
+import com.test.function.model.Student;
+
+import java.util.function.Supplier;
+
+/**
+ * Supplier use
+ *
+ * @author fengna
+ * @date 2019/3/26
+ */
+public class SupplierUse {
+    public static void main(String[] args) {
+        Supplier<Student> studentSupplier = () -> new Student();
+
+        System.out.println(studentSupplier.get().getName());
+
+        Supplier<Student> supplier = Student::new;
+
+        System.out.println(supplier.get().getAge());
+    }
+}
