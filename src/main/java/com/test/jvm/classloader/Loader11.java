57a0eb1195ae4f0e58a265055c7663aa2c81ec62
冯娜 <fengna@iwubida.com>
2019-05-06 14:43:44 +0800

    fix 自定义类加载器

--- src/main/java/com/test/jvm/classloader/Loader11.java
+++ src/main/java/com/test/jvm/classloader/Loader11.java
@@ -12,7 +12,9 @@ public class Loader11 extends ClassLoader{
 
     private String classLoaderName;
 
-    private final String classExtension = ".class";
+    private String path;
+
+    private final String fileExtension = ".class";
 
     public Loader11(String classLoaderName){
         super();
@@ -29,8 +31,15 @@ public class Loader11 extends ClassLoader{
         return "classLoaderName = " + classLoaderName;
     }
 
+    public void setPath(String path){
+        this.path = path;
+    }
+
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
+
+        System.out.println("findClass invoked : " + name);
+        System.out.println("class loader name : " + this.classLoaderName);
         byte[] data = loadClassData(name);
         return this.defineClass(name,data,0,data.length);
     }
@@ -40,10 +49,9 @@ public class Loader11 extends ClassLoader{
         InputStream is = null;
         byte[] data = null;
         ByteArrayOutputStream byteArrayOutputStream = null;
-
-        this.classLoaderName = this.classLoaderName.replace('.','/');
         try {
-            is = new FileInputStream(new File(name + this.classExtension));
+            this.classLoaderName = this.classLoaderName.replace(".","\\");
+            is = new FileInputStream(new File(this.path + name + this.fileExtension));
             byteArrayOutputStream = new ByteArrayOutputStream();
             int ch = 0;
             while(-1 != (ch = is.read())){
@@ -64,13 +72,25 @@ public class Loader11 extends ClassLoader{
     }
 
     public static void test(ClassLoader classLoader) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        Class<?> clazz = classLoader.loadClass("com.test.jvm.classloader.Loader11");
+        Class<?> clazz = classLoader.loadClass("com.test.jvm.classloader.Loader1");
         Object object = clazz.newInstance();
         System.out.println(object);
     }
 
     public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
-        Loader11 loader11 = new Loader11("loader11");
-        test(loader11);
+//        Loader11 loader1 = new Loader11("loader1");
+//        test(loader1);
+
+        Loader11 loader1 = new Loader11("loader1");
+        loader1.setPath("D:\\");
+        Class<?> clazz1 = loader1.loadClass("com.test.jvm.classloader.Loader1");
+        Object object1 = clazz1.newInstance();
+        System.out.println(object1);
+
+        Loader11 loader2 = new Loader11("loader2");
+        loader1.setPath("D:\\");
+        Class<?> clazz2 = loader1.loadClass("com.test.jvm.classloader.Loader1");
+        Object object2 = clazz1.newInstance();
+        System.out.println(object2);
     }
 }
