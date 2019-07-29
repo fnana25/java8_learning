38d341ec5c98f95e1a0775b0f427611ac3118e0f
冯娜 <fengna@iwubida.com>
2019-07-29 18:12:58 +0800

    注解

--- src/main/java/com/test/annotation/Test.java
+++ src/main/java/com/test/annotation/Test.java
@@ -6,9 +6,12 @@ package com.test.annotation;
  * @author may
  * @date 19/1/8 21:15
  */
+@MyAnnotation(value = "a",hello = "h",world = {"d","f"})
 public class Test {
 
-    public static void main(String[] args) {
-        String s= "";
+    @Deprecated
+    @MyAnnotation(value = "o",hello = "a",world = "q")
+    public void output(String message){
+        System.out.println(message);
     }
 }
